1e3a10f2e3bab6f9b5387bbae5233b1f5a035396
larisayagodina <cheeseroll@mail.ru>
2023-07-29 18:48:03 +0300

    init

---
+++ src/utils/taskFetching.js
@@ -0,0 +1,10 @@
+
+export async function taskFetching(fetchFunction, callback, errorMessage) {
+
+    try {
+        await fetchFunction()
+        await callback()
+    } catch(e) {
+        alert(errorMessage)
+    }
+}
